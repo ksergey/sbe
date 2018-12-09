906dc237bf915b56ecb62d1c1691e9a94a2fd47b
Sergey Kovalevich <inndie@gmail.com>
2018-12-09 21:44:16 +0300

    Update

--- cmake/SBEMakeCodec.cmake
+++ cmake/SBEMakeCodec.cmake
@@ -1,6 +1,7 @@
 find_package(Java COMPONENTS Runtime REQUIRED)
 
-function(sbe_make_codec output_dir schema target_ns target_var)
+function(sbe_make_codec schema target_ns target_var)
+    set(output_dir ${CMAKE_CURRENT_BINARY_DIR}/sbe-codecs/include)
     file(MAKE_DIRECTORY ${output_dir})
     set(output "${output_dir}/${target_ns}/MessageHeader.h")
     message(STATUS ${target_var})
